8bf354dcb9a5455ea6bac1b892f4f2cd8d7df471
VladSkopenko <skopirka2k17@gmail.com>
2024-07-27 07:50:23 +0300

    add two task

--- BestTime_to_Buy_andSeel.py
+++ BestTime_to_Buy_andSeel.py
@@ -6,4 +6,8 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ...
+        profit = 0
+        for i in range(0, len(prices) - 1):
+            if prices[i] < prices[i+1]:
+                profit += prices[i+1] - prices[i]
+        return profit
