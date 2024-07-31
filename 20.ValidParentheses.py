e37ab7a9d442060b91722809487d62b8e2a668c1
VladSkopenko <skopirka2k17@gmail.com>
2024-07-31 09:46:56 +0300

    add new task

---
+++ 20.ValidParentheses.py
@@ -0,0 +1,15 @@
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_brack = [] # Создал список для хранения открытых дужок
+        mapping = {')': '(', '}': '{', ']': '['} # Словарь соответсвий
+        for char in s:
+            if char in mapping:
+                top_element = open_brack.pop() if open_brack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                open_brack.append(char)
+
+        return not open_brack
