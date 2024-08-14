0bc827b51f501c98e895608bf32bf9caab986e36
VladSkopenko <skopirka2k17@gmail.com>
2024-08-14 16:44:54 +0300

    8 14.08

---
+++ 6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        current_row = 0
+        going_down = False
+
+        for char in s:
+            rows[current_row] += char
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            current_row += 1 if going_down else -1
+
+        return ''.join(rows)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.convert("PAYPALISHIRING", 3))
+    print(s.convert("PAYPALISHIRING", 4))
+    print(s.convert("A", 1))
---
+++ 8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # Шаг 1: Игнорируем начальные пробелы
+        s = s.lstrip()
+        if not s:
+            return 0
+
+        # Шаг 2: Определение знака числа
+        sign = 1
+        if s[0] == '-':
+            sign = -1
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+
+        # Шаг 3: Преобразование строки в число
+        result = 0
+        for char in s:
+            if char.isdigit():
+                result = result * 10 + int(char)
+            else:
+                break
+
+        # Шаг 4: Применение знака
+        result *= sign
+
+        # Шаг 5: Ограничение в пределах 32-битного целого числа
+        INT_MAX = 2 ** 31 - 1
+        INT_MIN = -2 ** 31
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+
+        return result
