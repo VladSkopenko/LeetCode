5385975285cc88c0132b7f127cb65b93895ae5e0
VladSkopenko <skopirka2k17@gmail.com>
2024-09-04 15:28:00 +0300

    resolve 11!!

--- 11.ContainerWithMostWater.py
+++ 11.ContainerWithMostWater.py
@@ -7,5 +7,24 @@ Return the maximum amount of water a container can store.
 
 Notice that you may not slant the container.
 
-
 """
+from typing import List
+
+
+class Solution:
+
+    def max_area(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+        while left < right:
+            current_height = min(height[left], height[right])
+            current_width = abs(left - right)
+            current_water = current_width * current_height
+            max_water = max(current_water, max_water)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
