1a3097a2f67a2d08125391572c3fc241df42c720
VladSkopenko <skopirka2k17@gmail.com>
2024-09-23 16:36:13 +0300

    28 task

---
+++ 28.py
@@ -0,0 +1,8 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+
+class Solution:
+    def str_str(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
